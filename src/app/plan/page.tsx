594fac9b349c0cd7b4e9dc6bad8f419505b0e1b0
Yasper De Jong <yasperdejong@gmail.com>
2023-02-23 02:18:53 -0800

    working fetch from database in plan/page

--- src/app/plan/page.tsx
+++ src/app/plan/page.tsx
@@ -1,7 +1,30 @@
+"use client";
+import { supabase } from "@/lib/supabase/utils/supabase-secret";
+import { useEffect, useState } from "react";
+
 export default function Home() {
-  return (
-    <main>
-      <div></div>
-    </main>
-  );
+    const [planData, setPlanData] = useState<any>(null);
+
+    const authsession = supabase.auth.getSession();
+    if (authsession) {
+        authsession.then((result) => {
+            console.log("Have session");
+            console.log(authsession);
+            const userId = result.data.session?.user.id;
+
+            if (userId) {
+                supabase
+                    .from("plans")
+                    .select("name, termId, created_at, updated_at, courses")
+                    .eq("userId", userId)
+                    .eq("name", "current")
+                    .then((result) => setPlanData(result));
+            }
+        });
+    }
+    return (
+        <main>
+            <div>{planData ? JSON.stringify(planData, null, 2) : ''}</div>
+        </main>
+    );
 }
