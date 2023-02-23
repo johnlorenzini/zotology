6ce26abaec1f14688b014345df595d3a212838c2
john lorenzini <lorenzinijd@gmail.com>
2023-02-22 17:29:51 -0800

    prerouting 2

--- src/scripts/socScraper.py
+++ src/scripts/socScraper.py
@@ -2,13 +2,13 @@ import requests
 import json
 import urllib.parse
 import time
- 
-formatted = dict()
 
 cleaned2 = []
 with open('formatted.json', 'r') as openfile:
     formatted = json.load(openfile)
-    for course in formatted:
-        tempcourse = course
-        for sections in course:
+    cleaned2 = sorted(formatted, key=lambda k: (k['deptCode'], k['courseNumber']))
+    
+    
+    with open('sorted.json', 'w') as outfile:
+        json.dump(cleaned2, outfile)
             
